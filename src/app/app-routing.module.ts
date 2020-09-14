a64f1e739b9d0ad49a869574bd0b5b4116484144
Juan Carlos Valdés <idevkingos@gmail.com>
2020-09-14 12:12:26 -0300

    angular bases - primera parte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,31 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+
+];
 
 @NgModule({
+  // un solo forRoot en toda la aplicacion
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
---
+++ src/app/components/menu/menu.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+  rutas =  [
+    { name: 'Home', path: '/home' },
+    { name: 'About', path: '/about' },
+    { name: 'Contact', path: '/contact' }
+  ];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
